6e1cb422f08101ae55c9587e7f8cc26fbae64ef3
whykyry <whykyry@gmail.com>
2025-07-14 12:50:34 -0700

    Fix asset paths and ensure .nojekyll is included in build

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '',
   server: {
     port: 1387,
     open: true,
@@ -16,6 +16,9 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: undefined,
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
       },
     },
   },
